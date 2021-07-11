a51ad51e07b4c5f5b9dbfd44470188a075426162
uditajain <67145364+uditajain@users.noreply.github.com>
2021-07-11 22:00:57 +0530

    Add files via upload

---
+++ LindaSchool/src/main/java/com/example/LindaSchool/LindaSchoolApplication.java
@@ -0,0 +1,13 @@
+package com.example.LindaSchool;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LindaSchoolApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LindaSchoolApplication.class, args);
+	}
+
+}
---
+++ LindaSchool/src/test/java/com/example/LindaSchool/LindaSchoolApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.LindaSchool;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LindaSchoolApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
